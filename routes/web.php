4b435eaec13bfa01b5156c8e11775a7d3df80497
Kristian Woolhouse <kwoolhouse@wonde.com>
2024-07-14 22:27:54 +0100

    Added Livewire, Alpinejs & Tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\Gallery;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Gallery::class)->name('gallery');
